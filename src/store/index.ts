f620b7dbe457292ec01b766a1df8f4c224bbc11b
harrygr <harry@grumbar.me>
2018-07-24 22:50:33 +0100

    Persist token in localstorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,23 @@
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore } from '../../node_modules/redux';
 import { reducer, State, Action } from './reducers';
+import { Option } from 'catling';
+import throttle = require('lodash/throttle');
+
+const retrieveState = () => {
+  return Option(localStorage.getItem('state'))
+    .map(JSON.parse)
+    .getOrElse({});
+};
 
 export const store = createStore<State, Action, {}, {}>(
   reducer,
-  {},
+  retrieveState(),
   composeWithDevTools(),
+);
+
+store.subscribe(
+  throttle(() => {
+    localStorage.setItem('state', JSON.stringify(store.getState()));
+  }, 1000),
 );
